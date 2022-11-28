578abe1326a88a4f8a8a74b67bcb413f6fefcbbe
TaiseiIto <taisei1212@outlook.jp>
2022-11-28 22:53:25 +0900

    vector

--- haskell/dynamics/test.hs
+++ haskell/dynamics/test.hs
@@ -1,6 +1,15 @@
 {-# OPTIONS -Wall -Werror #-}
 
+data Vector = Vector Float Float Float
+
+instance Show Vector
+ where
+  show (Vector x y z) = "(x = " ++ (show x) ++ ", y = " ++ (show y) ++ ", z = " ++ (show z) ++ ")"
+
 main :: IO ()
-main = do
- putStrLn "Hello, World!"
+main =
+ let
+  vector_a = Vector 1 2 3
+ in
+  putStrLn . ("vector_a = " ++) . show $ vector_a
 
