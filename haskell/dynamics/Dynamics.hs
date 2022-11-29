cd81e6362c47e5b99b774607676adec672f1c159
TaiseiIto <taisei1212@outlook.jp>
2022-11-29 10:26:52 +0900

    vector

--- haskell/dynamics/Dynamics.hs
+++ haskell/dynamics/Dynamics.hs
@@ -6,22 +6,23 @@ data Vector = Vector {x :: Float, y :: Float, z :: Float}
 
 infix 7 .*
 (.*) :: Vector -> Vector -> Float
-(Vector x0 y0 z0) .* (Vector x1 y1 z1) = x0 * x1 + y0 * y1 + z0 * z1
+v .* w = x v * x w + y v * y w + z v + z w
 
 veclen :: Vector -> Float
 veclen v = sqrt $ v .* v
 
 instance Num Vector
  where
-  (Vector x0 y0 z0) + (Vector x1 y1 z1) = Vector (x0 + x1) (y0 + y1) (z0 + z1)
-  (Vector x0 y0 z0) - (Vector x1 y1 z1) = Vector (x0 - x1) (y0 - y1) (z0 - z1)
-  (Vector x0 y0 z0) * (Vector x1 y1 z1) = Vector (y0 * z1 - z0 * y1) (z0 * x1 - x0 * z1) (x0 * y1 - y0 * x1)
+  v + w = Vector (x v + x w) (y v + y w) (z v + z w)
+  v - w = Vector (x v - x w) (y v - y w) (z v - z w)
+  v * w = Vector (y v * z w - z v * y w) (z v * x w - x v * z w) (x v * y w - y v * x w)
   negate v = Vector (- x v) (- y v) (- z v)
   abs v =  Vector (veclen v) 0 0
+  signum (Vector 0 0 0) = Vector 0 0 0
   signum v = Vector (x v / veclen v) (y v / veclen v) (z v / veclen v)
   fromInteger i = Vector ((fromInteger :: Integer -> Float) i) 0 0
 
 instance Show Vector
  where
-  show v = "(x = " ++ ((show . x) v) ++ ", y = " ++ ((show . y) v) ++ ", z = " ++ ((show . z) v) ++ ")"
+  show v = "(x = " ++ (show . x $ v) ++ ", y = " ++ (show . y $ v) ++ ", z = " ++ (show . z $ v) ++ ")"
 
--- haskell/dynamics/test.hs
+++ haskell/dynamics/test.hs
@@ -6,6 +6,17 @@ main :: IO ()
 main =
  let
   vector_a = Dynamics.Vector 1 2 3
- in
+  vector_b = Dynamics.Vector 3 2 1
+ in do
   putStrLn . ("vector_a = " ++) . show $ vector_a
+  putStrLn . ("vector_b = " ++) . show $ vector_b
+  putStrLn . ("vector_a + vector_b = " ++) . show $ vector_a + vector_b
+  putStrLn . ("vector_a - vector_b = " ++) . show $ vector_a - vector_b
+  putStrLn . ("vector_a * vector_b = " ++) . show $ vector_a * vector_b
+  putStrLn . ("negate vector_a = " ++) . show . negate $ vector_a
+  putStrLn . ("abs vector_a = " ++) . show . abs $ vector_a
+  putStrLn . ("signum $ Dynamics.Vector 0 0 0 = " ++) . show . signum $ Dynamics.Vector 0 0 0
+  putStrLn . ("signum vector_a = " ++) . show . signum $ vector_a
+  putStrLn . ("abs . signum $ vector_a = " ++) . show . abs . signum $ vector_a
+  putStrLn . ("(fromInteger :: Integer -> Dynamics.Vector) 1 = " ++). show . (fromInteger :: Integer -> Dynamics.Vector) $ 1
 
